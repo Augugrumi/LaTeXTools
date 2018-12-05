c4aabfb19f5edad7d1986dfe608505e176f18467
Davide Polonio <poloniodavide@gmail.com>
2018-12-05 10:28:56 +0100

    Add flexibility to spellcheck script - Add possibility to set the min and max tex number via NUM_MIN_SCHECK and NUM_MAX_SCHECK variables - Add a second argument to specify a custom tex sources location - Ensured retro-compatibility

--- spellchecker/spellcheck.sh
+++ spellchecker/spellcheck.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
 reporoot=$(git rev-parse --show-toplevel)
-for i in "$reporoot"/res/sections/*.tex
+if [ -z "$NUM_MIN_SCHECK" ]
+then
+    min=6
+else
+    min=$NUM_MIN_SCHECK
+fi
+if [ -z "$NUM_MAX_SCHECK" ]
+then
+    max=90
+else
+    max=$NUM_MAX_SCHECK
+fi
+if [ -z "$2" ]
+then
+    texdir="/res/sections/*.tex"
+else
+    texdir=$2
+fi
+echo "Checking files in $reporoot$texdir"
+for i in $reporoot$texdir
 do
     filebasename=$(basename $i)
     filenumber=$(echo "$filebasename" | cut -f1 -d"-")
-    if [ "$filenumber" -gt "6" ]  && [ "$filenumber" -lt "90" ]
+    if [ "$filenumber" -gt "$min" ]  && [ "$filenumber" -lt "$max" ]
     then
-	echo "Checking file $filebasename"
-	tmp=$(cat $i | aspell -t -a --lang=$1 --encoding=utf-8 --add-extra-dicts="$reporoot/dictionary.$1.pws" | grep -v "*" | grep -v "@" | grep -v "+" | sed '/^\s*$/d')
-	echo "List of errors detected for $filebasename"
-	echo "$tmp"
-	echo "End of list for $filebasename"
-	res=$res$tmp
+	      echo "Checking file $filebasename"
+	      tmp=$(cat $i | aspell -t -a --lang=$1 --encoding=utf-8 --add-extra-dicts="$reporoot/dictionary.$1.pws" | grep -v "*" | grep -v "@" | grep -v "+" | sed '/^\s*$/d')
+	      echo "List of errors detected for $filebasename"
+	      echo "$tmp"
+	      echo "End of list for $filebasename"
+	      res=$res$tmp
     fi
 done
 
